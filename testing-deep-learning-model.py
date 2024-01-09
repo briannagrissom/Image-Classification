de571f7b6301c9179c11ba84da43862949935fa8
Brianna Grissom <114755509+briannagrissom@users.noreply.github.com>
2024-01-09 13:35:27 -0800

    Update and rename testing-deep-learning-model to testing-deep-learning-model.py

--- testing-deep-learning-model.py
+++ testing-deep-learning-model.py
@@ -1,7 +1,3 @@
-
-
-
-
 from keras.models import load_model
 import os
 import matplotlib.pyplot as plt
@@ -9,54 +5,51 @@ import cv2
 import numpy as np
 import urllib.request
 
-# function to retrieve an image from the internet
+# Define function to retrieve an image from the internet
 def get_image(url):
-    req = urllib.request.urlopen(url)
-    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-    img = cv2.imdecode(arr, -1)
-    return img
-
-
+    request = urllib.request.urlopen(url)
+    array = np.asarray(bytearray(request.read()), dtype=np.uint8)
+    image = cv2.imdecode(array, -1)
+    return image
 
 link= input('Insert path to image: ')
 species= input('Insert the species: ').upper()
 
+# Obtain the image and resize it
 img= get_image(f'{link}')
 img= cv2.resize(img, (170, 170))
-
 
 # load in the model
 model= load_model('ml-models/imagemodelNEW.h5') 
 
-
-y= []
-y.append(img)
-y= np.array(y)
+# expand the dimensions
+img_new = []
+img_new.append(img)
+img_new = np.array(img_new)
 
 # use the model to predict what species the image is
-prediction_array= model.predict(y)
+prediction_array= model.predict(img_new)
 predicted= np.argmax(prediction_array)
 
 # map numerical values to species names
 classes= {0:'COW', 1:'CAT', 2:'CROCODILE', 3:'CHAMELEON', 4:'DRAGONFLY'}
 
-# display the image and say if the model predicted correctly or incorrectly
+# specify whether the model predicted correctly or incorrectly and display the image
 if classes[predicted] == species:
     print(f'Model CORRECTLY predicted the class of {classes[predicted]}')
 if classes[predicted] != species:
     print(f'Model was INCORRECT. It predicted the class of {classes[predicted]}')
+plt.imshow(img_new[0])
 
-plt.imshow(y[0])
-
-
-# record the amount of times the model was correct/incorrect to a TXT file
+# record if the model was correct/incorrect to a TXT file
+if classes[predicted] == species:
+    with open('Predicted Correctly','a') as file:
+        file.write(f'predicted correctly: {classes[predicted]}')
 if classes[predicted] != species:
-    with open('Number Predicted Incorrect','a') as file:
+    with open('Predicted Incorrectly','a') as file:
         file.write(f'predicted: {classes[predicted]}, actual species: {species}\n')
     
-if classes[predicted] == species:
-    with open('Number Predicted Correct','a') as file:
-        file.write(f'predicted: {classes[predicted]}, actual species: {species}\n')
+
         
         
         
